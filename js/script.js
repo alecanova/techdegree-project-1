5756d4f89902abd71057abb7be5ff3df1b256a6e
Alessandro Canova <alessandrocanova14@gmail.com>
2020-05-03 20:12:32 +0200

    Created quotes array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,50 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+let quotes = [
+  {
+    quote: "If I could say it in words there would be no reason to paint.",
+    source: "Edward Hopper"
+  },
+  {
+    quote: "Play by the rules, but be ferocious.",
+    source: "Phil Knight"
+  },
+  {
+    quote: "There's no way around hard work. Embrace it.",
+    source: "Roger Federer",
+    citation: "Lessons from the court",
+    year: 2013
+  },
+  {
+    quote: "It is extremely difficult to stay alert and attentive instead of getting hypnotized by the constant monolog inside your head.",
+    source: "Davis Foster Wallace",
+    citation: "This is water speech",
+    year: 2005
+  },
+  {
+    quote: "Ideas are like fish. If you want to catch little fish, you can stay in the shallow water. But if you want to catch the big fish, you've got to go deeper. Down deep, the fish are more powerful and more pure. They're huge and abstract. And they're very beautiful.",
+    source: "David Lynch",
+    citation:  "Catching the Big Fish: Meditation, Consciousness, and Creativity",
+    year: 2006
+  },
+  {
+    quote: "As soon as you start talking about mistique, you have none.",
+    source: "Neil Young"
+  },
+  {
+    quote: "For me it's all about performance. Nothing good happens in the world by being happy and cosy. Nobody achieves anything great because they're happy and cosy.",
+    source: "Alex Honnold",
+    citation: "Free solo",
+    year: 2017
+  },
+  {
+    quote: "Choose your habits well. Habit is probably the most powerful tool in your brain's toolbox.",
+    source: "Ray Dalio",
+    citation: "Principles",
+    year: 2017
+  }
+];
 
 
 /***
